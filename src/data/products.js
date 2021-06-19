061c6991df7a7b3d1334f8ec5b672e3e6667e1dc
abhishektanwar <mithyalabs@Abhisheks-MacBook-Pro.local>
2021-06-19 16:28:15 +0530

    dummy data

--- src/data/products.js
+++ src/data/products.js
@@ -3,27 +3,27 @@ export const products = [{
   "title" : "FLYING MACHINE T-Shirt",
   "category":"Men",
   "brand":"Flying Machine",
-  "price": 355,
+  "price": 499,
   "size" : "S M L XL",
-  "imageUrl" : "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg"
+  "imageUrl" : "https://rukminim1.flixcart.com/image/714/857/khmbafk0-0/t-shirt/w/j/s/xs-t322-blwh-seven-rocks-original-imafxhagtyj2xvpn.jpeg?q=50"
 },
 {
     "id" : 1,
   "title" : "Van Heusen Casual Slim Fit",
   "category":"Men",
   "brand":"Van Heusen",
-  "price": 539,
+  "price": 345,
   "size" : "S M",
-  "imageUrl": "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
+  "imageUrl": "https://rukminim1.flixcart.com/image/714/857/kmns7m80/shirt/z/n/z/42-pcsfsslbw51230-peter-england-original-imagfg9vzjeefnfx.jpeg?q=50",
 },
 {
     "id" : 2,
   "title" : "FLYING MACHINE Women's 3-in-1 Snowboard Jacket Winter Coats",
   "category": "Women",
   "brand":"Flying Machine",
-  "price": 579,
+  "price": 689,
   "size" : "S M L XL",
-  "imageUrl":"https://fakestoreapi.com/img/51Y5NI-I5jL._AC_UX679_.jpg",
+  "imageUrl":"https://rukminim1.flixcart.com/image/714/857/kgv5x8w0/jacket/y/n/q/xxl-8398575-quechua-original-imafxykjhbccqbcv.jpeg?q=50",
 },
 {
     "id" : 3,
@@ -32,7 +32,7 @@ export const products = [{
   "brand":"WildCraft",
   "price": 449,
   "size" : "M",
-  "imageUrl":"https://fakestoreapi.com/img/51eg55uWmdL._AC_UX679_.jpg",
+  "imageUrl":"https://rukminim1.flixcart.com/image/714/857/ko382a80/top/w/y/q/xl-white-stripped1-ryte-style-original-imag2mpjg7attyfg.jpeg?q=50",
 },
 {
     "id" : 4,
@@ -50,7 +50,7 @@ export const products = [{
   "brand":"Jaffrey",
   "price": 999,
   "size" : "S",
-  "imageUrl" : "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  "imageUrl" : "https://rukminim1.flixcart.com/image/714/857/kf75fgw0/backpack/u/z/5/walker-office-backpack-bag-bag-school-bag-laptop-bag-04-laptop-original-imafvpcjrgqdgbce.jpeg?q=50",
 },
 {
     "id": 6,
@@ -59,7 +59,7 @@ export const products = [{
     "brand":"Sara",
     "price" : 350,
     "size":"S",
-    "imageUrl": "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg",
+    "imageUrl": "https://rukminim1.flixcart.com/image/714/857/jo1s8sw0/bangle-bracelet-armlet/s/9/9/free-size-1-ybbn-91340a-you-bella-original-imafakzzu2zdh8zq.jpeg?q=50",
 },
 {
     "id":7,
@@ -68,6 +68,6 @@ export const products = [{
     "category":"Women",
     "price":1999,
     "size":"S M",
-    "imageUrl":"https://fakestoreapi.com/img/71HblAHs5xL._AC_UY879_-2.jpg"
+    "imageUrl":"https://rukminim1.flixcart.com/image/714/857/khdqnbk0/raincoat/l/f/z/free-pink-new-raincoat-tryo-original-imafuzbzqvnx74nk.jpeg?q=50"
 }
 ];
